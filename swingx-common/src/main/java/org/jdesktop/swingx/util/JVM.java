a7d81f36c4f97e7b1689d85b2595cd7f588f89f4
swimmesberger <wimmesberger@gmail.com>
2018-11-29 17:17:59 +0100

    Fixed java version detection

--- swingx-common/src/main/java/org/jdesktop/swingx/util/JVM.java
+++ swingx-common/src/main/java/org/jdesktop/swingx/util/JVM.java
@@ -38,6 +38,8 @@ public class JVM {
   public final static int JDK1_6 = 1600;
   public final static int JDK1_6N = 1610;
   public final static int JDK1_7 = 1700;
+  public final static int JDK1_8 = 1800;
+  public final static int JDK1_9 = 1900;
 
   private static JVM current;
   static {
@@ -66,7 +68,13 @@ public class JVM {
    * Constructor for the OS object
    */
   public JVM(String p_JavaVersion) {
-    if (p_JavaVersion.startsWith("1.7.")) {
+	if(!p_JavaVersion.startsWith("1.")) {
+      // if the java version is higher than JDK 8 the version string does not start with 1. anymore
+      // we assume here we encountered a JDK 9
+      jdkVersion = JDK1_9;
+	} else if(p_JavaVersion.startsWith("1.8.")) {
+	  jdkVersion = JDK1_8;
+	} else if (p_JavaVersion.startsWith("1.7.")) {
       jdkVersion = JDK1_7;
     } else if (p_JavaVersion.startsWith("1.6.")) {
       for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
@@ -136,5 +144,8 @@ public class JVM {
   public boolean isOneDotSeven() {
       return jdkVersion == JDK1_7;
   }
-
+  
+  public boolean isOneDotEight() {
+      return jdkVersion == JDK1_8;
+  }
 }
